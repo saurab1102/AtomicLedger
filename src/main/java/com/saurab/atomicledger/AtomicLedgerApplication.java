04f0d5f299c946286cc557b09d679fd2999b166b
saurab1102 <saurab1102@gmail.com>
2026-05-07 23:25:02 +0530

    Initialize Spring Boot service

---
+++ src/main/java/com/saurab/atomicledger/AtomicLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.saurab.atomicledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicLedgerApplication.class, args);
+	}
+
+}
